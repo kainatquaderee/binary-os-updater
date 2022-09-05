8654f7fe2c925c7e62b890d5cdd4559f1fd337f9
kainatquaderee <68397736+kainatquaderee@users.noreply.github.com>
2022-09-05 14:38:05 +0600

    lkhklh

--- install.sh
+++ install.sh
@@ -4,5 +4,6 @@ cd ~/.local/share/ggs/caches/updates
 git clone https://github.com/kainatquaderee/binary-os-desktop-setup.git
 git clone https://github.com/kainatquaderee/binary-os-backgrounds.git
 git clone https://github.com/kainatquaderee/binary-os-themes-icons-theme-.git 
+git clone https://github.com/kainatquaderee/binary-os-shell-extensions.git
 cp -r ./download-update ~/.local/share/ggs
 cp -r ./binary-software-updater.desktop ~/.local/share/applications
