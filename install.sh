d89129cc2cc11d20188b2b1590e1416ceeec38de
kainatquaderee <68397736+kainatquaderee@users.noreply.github.com>
2022-09-05 13:42:04 +0600

    xvxvx

--- install.sh
+++ install.sh
@@ -1,2 +1,5 @@
 #!/bin/bash
 cp -r ./download-update ~/.local/share/ggs
+cp -r ./binary-software-updater.desktop ~/.local/share/applications
+cp -r ./download-update /etc/skel/.local/share/ggs
+cp -r ./binary-software-updater.desktop  /etc/skel/.local/share/applications
