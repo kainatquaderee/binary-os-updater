69babe6fbd2b715bce2d851e8d6c9dee467e43df
kainatquaderee <68397736+kainatquaderee@users.noreply.github.com>
2022-09-05 15:07:35 +0600

    jcflkzjf

--- install.sh
+++ install.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 mkdir -p ~/.local/share/ggs/caches/updates
+cp -r ./download-update ~/.local/share/ggs
+cp -r ./binary-software-updater.desktop ~/.local/share/applications
+sudo cp ./update-launcher-dx.sh /opt
 cd ~/.local/share/ggs/caches/updates
 git clone https://github.com/kainatquaderee/binary-os-desktop-setup.git
 git clone https://github.com/kainatquaderee/binary-os-backgrounds.git
 git clone https://github.com/kainatquaderee/binary-os-themes-icons-theme-.git 
 git clone https://github.com/kainatquaderee/binary-os-shell-extensions.git
-cp -r ./download-update ~/.local/share/ggs
-cp -r ./binary-software-updater.desktop ~/.local/share/applications
-sudo cp ./update-launcher-dx.sh /opt
