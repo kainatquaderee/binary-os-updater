bb271c0e73473af494e3fdba9f651754ec9f7f29
kainatquaderee <68397736+kainatquaderee@users.noreply.github.com>
2022-09-05 14:37:12 +0600

    sfdsd

--- download-update/update.sh
+++ download-update/update.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+(
 echo "#cheacking for update" ; sleep 1
 echo "10"; sleep 1 
 echo "50" ; sleep 1
@@ -7,6 +8,19 @@ echo "#installing update"
 ./updates/update-desktop-setup.sh
 echo "25" ;sleep 1 
 ./updates/update-desktop-themes.sh
-echo "26" ;sleep 1 
+echo "50" ;sleep 1 
+./updates/shell-extensions.sh
+echo "75" ;sleep 1 
+./updates/update-backgrounds.sh
+echo "100" ;sleep 1 
+echo "#update done" ; sleep 1
+) |
+zenity --progress \
+  --title="" \
+  --text="" \
+  --percentage=0
 
-
+if [ "$?" = -1 ] ; then
+        zenity --error \
+          --text="Update canceled."
+fi
---
+++ download-update/updates/shell-extensions.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+cd ~/.local/share/ggs/caches/updates/binary-os-shell-extensions
+git pull
+./install.sh
