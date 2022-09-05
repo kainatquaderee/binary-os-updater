99365393bc47a9e196a32bd755d2bae45bb9bb44
kainatquaderee <68397736+kainatquaderee@users.noreply.github.com>
2022-09-05 18:45:00 +0600

    sdsada

--- download-update/update.sh
+++ download-update/update.sh
@@ -1,20 +1,19 @@
 #!/bin/bash
-cd ~/.local/share/ggs/download-update
 (
 echo "#cheacking for update" ; sleep 1
 echo "10"; sleep 1 
 echo "50" ; sleep 1
 echo "100" ; sleep 1
 echo "#installing update"
-./updates/update-desktop-setup.sh
+~/.local/share/ggs/download-update/updates/update-desktop-setup.sh
 echo "25" ;sleep 1 
-./updates/update-desktop-themes.sh
+~/.local/share/ggs/download-update/updates/update-desktop-themes.sh
 echo "50" ;sleep 1 
-./updates/install-update.sh
+~/.local/share/ggs/download-update/updates/install-update.sh
 echo "60" ;sleep 1 
-./updates/shell-extensions.sh
+~/.local/share/ggs/download-update/updates/shell-extensions.sh
 echo "75" ;sleep 1 
-./updates/update-backgrounds.sh
+~/.local/share/ggs/download-update/updates/update-backgrounds.sh
 echo "90" ;sleep 1 
 echo "#update done" ; sleep 1
 /usr/bin/update-manager
