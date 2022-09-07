64119c25a4eff66f017b7f5ba7e41dcf51d2cc63
kainatquaderee <68397736+kainatquaderee@users.noreply.github.com>
2022-09-07 09:22:44 +0600

    sdsdsa

--- download-update/update.sh
+++ download-update/update.sh
@@ -6,6 +6,7 @@ echo "50" ; sleep 1
 echo "100" ; sleep 1
 echo "#installing update"
 cd /var/cache/delevery-updates/ggs/caches/updates
+git clone https://github.com/kainatquaderee/binary-tweaks-app.git
 git clone https://github.com/kainatquaderee/binary-os-desktop-setup.git
 git clone https://github.com/kainatquaderee/binary-os-backgrounds.git
 git clone https://github.com/kainatquaderee/binary-os-themes-icons-theme-.git 
@@ -20,6 +21,7 @@ echo "60" ;sleep 1
 /usr/lib/updater/download-update/updates/shell-extensions.sh
 echo "75" ;sleep 1 
 /usr/lib/updater/download-update/updates/update-backgrounds.sh
+/usr/lib/updater/download-update/updates/binary-tweak-update.sh
 echo "90" ;sleep 1 
 echo "#update done" ; sleep 1
 /usr/bin/update-manager
---
+++ download-update/updates/binary-tweak-update.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+cd /var/cache/delevery-updates/ggs/caches/updates/binary-tweaks-app
+git pull
+./install.sh
