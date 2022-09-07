90407f584a8fa848c013ee49e20248ed39ab4f0b
kainatquaderee <68397736+kainatquaderee@users.noreply.github.com>
2022-09-07 08:51:04 +0600

    p';l'

--- download-update/updates/install-update.sh
+++ download-update/updates/install-update.sh
@@ -4,4 +4,3 @@ git pull
 cp -r ./download-update ~/.local/share/ggs
 cp -r ./binary-software-updater.desktop ~/.config/autostart
 cp -r ./binary-software-updater.desktop ~/.local/share/applications
-sudo cp ./update-launcher-dx.sh /opt
