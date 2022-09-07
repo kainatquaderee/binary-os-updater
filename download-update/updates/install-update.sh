19d503088a2bd9b90056fc994617fbd789ab7d2d
kainatquaderee <68397736+kainatquaderee@users.noreply.github.com>
2022-09-07 09:01:59 +0600

    sdsd

--- download-update/updates/install-update.sh
+++ download-update/updates/install-update.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 cd /var/cache/delevery-updates/ggs/caches/updates/binary-os-updater
 git pull 
-cp -r ./download-update ~/.local/share/ggs
+cp -r ./download-update /usr/lib/updater
 cp -r ./binary-software-updater.desktop ~/.config/autostart
 cp -r ./binary-software-updater.desktop ~/.local/share/applications
