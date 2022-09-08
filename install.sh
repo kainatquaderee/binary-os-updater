34bb0474627bb84346a5d1a6cc0b3331ff5c875a
kainatquaderee <68397736+kainatquaderee@users.noreply.github.com>
2022-09-08 21:41:57 +0600

    sdsadsad

--- install.sh
+++ install.sh
@@ -1,6 +1,17 @@
 #!/bin/bash
-mkdir -p /var/cache/delevery-updates/ggs/caches/updates
-mkdir -p /usr/lib/updater
+sudo mkdir -p /var/cache/delevery-updates/ggs/caches/updates
+sudo chmod -R 777  /var/cache/delevery-updates
+sudo mkdir -p /usr/lib/updater
+sudo chmod -R 777 /usr/lib/updater
+sudo chmod -R 777 /usr/share/themes
+sudo chmod -R 777 /usr/share/icons
+sudo chmod -R 777 /usr/libexec/gos
+sudo chmod -R -777 /usr/libexec/initial-setup
+sudo chmod -R -777 /usr/share/backgrounds
+sudo chmod -R -777 /usr/share/gnome-background-properties
+sudo chmod -R -777 /usr/share/pixmaps
+sudo chmod -R -777 /usr/share/plymouth
+mkdir
 cp -r ./download-update /usr/lib/updater
 cp -r ./binary-software-updater.desktop ~/.local/share/applications
 sudo cp ./update-launcher-dx.sh /opt
