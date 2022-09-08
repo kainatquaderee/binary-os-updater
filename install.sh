f38286e0b172db80062c44e9cdfbed5dd1b86f59
kainatquaderee <68397736+kainatquaderee@users.noreply.github.com>
2022-09-08 22:00:45 +0600

    sdsdfasd

--- install.sh
+++ install.sh
@@ -6,12 +6,12 @@ sudo chmod -R 777 /usr/lib/updater
 sudo chmod -R 777 /usr/share/themes
 sudo chmod -R 777 /usr/share/icons
 sudo chmod -R 777 /usr/libexec/gos
-sudo chmod -R -777 /usr/libexec/initial-setup
-sudo chmod -R -777 /usr/share/backgrounds
-sudo chmod -R -777 /usr/share/gnome-background-properties
-sudo chmod -R -777 /usr/share/pixmaps
-sudo chmod -R -777 /usr/share/plymouth
-sudo chmod -R -777 /opt/binary-theming
+sudo chmod -R 777 /usr/libexec/initial-setup
+sudo chmod -R 777 /usr/share/backgrounds
+sudo chmod -R 777 /usr/share/gnome-background-properties
+sudo chmod -R 777 /usr/share/pixmaps
+sudo chmod -R 777 /usr/share/plymouth
+sudo chmod -R 777 /opt/binary-theming
 mkdir
 cp -r ./download-update /usr/lib/updater
 cp -r ./binary-software-updater.desktop ~/.local/share/applications
