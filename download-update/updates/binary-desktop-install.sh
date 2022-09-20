0b92fe2f33f5a2837d41a7fd3e082590342f507f
kainatquaderee <68397736+kainatquaderee@users.noreply.github.com>
2022-09-20 15:03:16 +0600

    sdasdas

--- download-update/updates/binary-desktop-install.sh
+++ download-update/updates/binary-desktop-install.sh
@@ -2,4 +2,4 @@
 cd /var/cache/delevery-updates/ggs/caches/updates/Binary-desktop/
 git pull
 cd binary-desktop
-apt install binary-desktop_1%3a42+3_amd64..deb
+apt install ./binary-desktop_1%3a42+3_amd64..deb
