2cae83cf223c38086fc8453c706f367cfdf1c161
kainatquaderee <68397736+kainatquaderee@users.noreply.github.com>
2022-09-22 17:48:29 +0600

    scsZdsd

--- download-update/update.sh
+++ download-update/update.sh
@@ -7,31 +7,31 @@ echo "10"; sleep 1
 echo "50" ; sleep 1
 echo "100" ; sleep 1
 echo "#installing update"
-mkdir -p /var/cache/delevery-updates/ggs/caches/updates
+echo $password | sudo -S mkdir -p /var/cache/delevery-updates/ggs/caches/updates
 cd /var/cache/delevery-updates/ggs/caches/updates
-git clone https://github.com/kainatquaderee/binary-tweaks-app.git
-git clone https://github.com/kainatquaderee/binary-os-desktop-setup.git
-git clone https://github.com/kainatquaderee/binary-os-backgrounds.git
-git clone https://github.com/kainatquaderee/binary-os-themes-icons-theme-.git 
-git clone https://github.com/kainatquaderee/binary-os-shell-extensions.git
-git clone https://github.com/kainatquaderee/binary-os-updater.git
-git clone https://github.com/kainatquaderee/Binary-desktop.git
-git clone https://github.com/kainatquaderee/binary-commands.git
-git clone https://github.com/kainatquaderee/binary-base-files.git
-git clonehttps://github.com/kainatquaderee/binary-os-fonts-roboto-.git
-git clone https://github.com/kainatquaderee/binary-plymouth-theme.git
-git clone https://github.com/kainatquaderee/binary-os-release-upgrader.git
-/usr/lib/updater/download-update/updates/update-desktop-setup.sh
+echo $password | sudo -S git clone https://github.com/kainatquaderee/binary-tweaks-app.git
+echo $password | sudo -S git clone https://github.com/kainatquaderee/binary-os-desktop-setup.git
+echo $password | sudo -S git clone https://github.com/kainatquaderee/binary-os-backgrounds.git
+echo $password | sudo -S git clone https://github.com/kainatquaderee/binary-os-themes-icons-theme-.git 
+echo $password | sudo -S git clone https://github.com/kainatquaderee/binary-os-shell-extensions.git
+echo $password | sudo -S git clone https://github.com/kainatquaderee/binary-os-updater.git
+echo $password | sudo -S git clone https://github.com/kainatquaderee/Binary-desktop.git
+echo $password | sudo -S git clone https://github.com/kainatquaderee/binary-commands.git
+echo $password | sudo -S git clone https://github.com/kainatquaderee/binary-base-files.git
+echo $password | sudo -S git clonehttps://github.com/kainatquaderee/binary-os-fonts-roboto-.git
+echo $password | sudo -S git clone https://github.com/kainatquaderee/binary-plymouth-theme.git
+echo $password | sudo -S git clone https://github.com/kainatquaderee/binary-os-release-upgrader.git
+echo $password | sudo -S /usr/lib/updater/download-update/updates/update-desktop-setup.sh
 echo "25" ;sleep 1 
-/usr/lib/updater/download-update/updates/update-desktop-themes.sh
+. /usr/lib/updater/download-update/updates/update-desktop-themes.sh
 echo "50" ;sleep 1 
 echo $password | sudo -S /usr/lib/updater/download-update/updates/install-update.sh
 echo "60" ;sleep 1 
-/usr/lib/updater/download-update/updates/shell-extensions.sh
+. /usr/lib/updater/download-update/updates/shell-extensions.sh
 echo "75" ;sleep 1 
-/usr/lib/updater/download-update/updates/update-backgrounds.sh
-/usr/lib/updater/download-update/updates/binary-tweak-update.sh
-/usr/lib/updater/download-update/updates/font-update.sh
+. /usr/lib/updater/download-update/updates/update-backgrounds.sh
+. /usr/lib/updater/download-update/updates/binary-tweak-update.sh
+echo $password | sudo -S /usr/lib/updater/download-update/updates/font-update.sh
 echo $password | sudo -S /usr/lib/updater/download-update/updates/binary-os-release-upgrader.sh
 echo $password | sudo -S /usr/lib/updater/download-update/updates/binary-plymouth-theme.sh
 echo $password | sudo -S /usr/lib/updater/download-update/updates/binary-base-files.sh
