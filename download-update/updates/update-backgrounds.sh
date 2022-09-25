618199c861e3fdffcc1bd18dd37d91fb3d064158
kainatquaderee <68397736+kainatquaderee@users.noreply.github.com>
2022-09-25 08:52:02 +0600

    Gc

--- download-update/updates/update-backgrounds.sh
+++ download-update/updates/update-backgrounds.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 cd /var/cache/delevery-updates/ggs/caches/updates/binary-os-backgrounds
 echo $password | sudo -S  git pull
-. /var/cache/delevery-updates/ggs/caches/updates/binary-os-backgrounds/install.sh
+source /var/cache/delevery-updates/ggs/caches/updates/binary-os-backgrounds/install.sh
