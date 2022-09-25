fee9e51136a89d584ad37e2beffd596b0b865bef
kainatquaderee <68397736+kainatquaderee@users.noreply.github.com>
2022-09-25 08:49:21 +0600

    Yu

--- download-update/updates/update-desktop-themes.sh
+++ download-update/updates/update-desktop-themes.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 cd /var/cache/delevery-updates/ggs/caches/updates/binary-os-themes-icons-theme-
 echo $password | sudo -S  git pull
-. ./icons-and-themes/install.sh
+source ./icons-and-themes/install.sh
